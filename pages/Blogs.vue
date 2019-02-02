9e93c99c25a6beaf648e493a22a7b1dc468ac0c4
Hiras <mhmdhiras@gmail.com>
2019-02-02 13:51:51 +0530

    initial commit

---
+++ pages/Blogs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="blogContainer">
+        <h1>All Blogs</h1>
+        <div>
+            <p>
+                The space wil be filled with wonderful blog posts
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.blogContainer {
+    text-align: center;
+}
+</style>
